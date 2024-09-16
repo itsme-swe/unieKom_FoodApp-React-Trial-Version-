99b31af55cafcea67b082f2dc5055c100c7b4032
itsme-swe <me.harshmehra07@gmail.com>
2024-09-16 07:57:07 +0530

    update

--- App.js
+++ App.js
@@ -1,23 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-const jsxElement = <h1 className="ele1">I am JSX Element</h1>;
-
-// Functional Component
-const HeadingComponent = function () {
-  return <h2>I am functional component</h2>;
-};
-
-// Functional Component using Arrow function
-const Title = () => {
-  return (
-    <h1 className="head" tabIndex="5">
-      {jsxElement}
-      <HeadingComponent />
-      Functional Component using Arrow Function
-    </h1>
-  );
+// Outermost Element, aub app ki joh bhi components ayenge sub iskae andar ayenge
+const AppLayout = () => {
+  return <div className="app"></div>;
 };
 
 const root = ReactDOM.createRoot(document.querySelector(".head"));
