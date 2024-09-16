0c380db3cfcdc973532a488149de49fae64650b8
itsme-swe <me.harshmehra07@gmail.com>
2024-09-16 22:48:01 +0530

    update

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import img1 from "/logo.jpg";
 
-// Outermost Element, aub app ki joh bhi components ayenge sub iskae andar ayenge
-const AppLayout = () => {
-  return <div className="app"></div>;
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={img1} alt="logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-const root = ReactDOM.createRoot(document.querySelector(".head"));
-root.render(<Title />);
+// Outermost Element, aub puri app mae joh bhi components ayenge sub iskae andar ayenge
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <HeaderComponent />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.querySelector(".root"));
+root.render(<AppLayout />);
