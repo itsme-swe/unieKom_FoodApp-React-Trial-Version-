e05f7922a8582b2c666506cf0dcb67c892641a27
itsme-swe <me.harshmehra07@gmail.com>
2024-09-19 18:44:39 +0530

    update

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,10 +1,10 @@
-import img1 from "/logo.jpg";
+import { LOGO_URL } from "../utils/constants";
 
 const HeaderComponent = () => {
   return (
     <div className="header">
       <div className="logo-container">
-        <img className="logo" src={img1} alt="logo" />
+        <img className="logo" src={LOGO_URL} alt="logo" />
       </div>
       <div className="nav-items">
         <ul>
